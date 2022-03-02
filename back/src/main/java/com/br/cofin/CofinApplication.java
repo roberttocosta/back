68bdce9ac449713e17aa06dd33512f9de105d7e0
Roberto Costa <roberto.costa@tse.jus.br>
2022-03-01 22:31:27 -0300

    Initial commit

---
+++ back/src/main/java/com/br/cofin/CofinApplication.java
@@ -0,0 +1,13 @@
+package com.br.cofin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CofinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CofinApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/br/cofin/CofinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.cofin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
